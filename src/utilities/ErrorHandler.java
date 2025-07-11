fe3025221005cd9a7162a53251e39e1f8fd7f691
sahsit <salarhaider03@gmail.com>
2025-07-11 19:31:21 -0400

    Errorhandler terminates the program after error is logged

--- src/utilities/ErrorHandler.java
+++ src/utilities/ErrorHandler.java
@@ -6,6 +6,7 @@ public class ErrorHandler {
     // Handles errors by printing them to the console
     public void handle(String error) {
         System.err.println("Error: " + error);
+        System.exit(1); 
     }
 
     // Handles exceptions by converting them to strings and passing them to the handle method
